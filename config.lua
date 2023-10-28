4826826dec190b23ed858c6d0c59df02cce68a39
Christian Oaksford <39574029+coaksford@users.noreply.github.com>
2023-10-28 02:04:19 -0500

    Fix keymapping, add folding.

--- config.lua
+++ config.lua
@@ -4,24 +4,25 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 vim.o.timeoutlen = 200 --milliseconds
 
+vim.o.relativenumber = true
+vim.opt.scrolloff = 10
+vim.o.tabstop = 4
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 
+lvim.keys.normal_mode["<leader>tt"] = "<cmd>ToggleTerm size=80 direction=float<Enter>"
+lvim.keys.normal_mode["<leader>ts"] = "<cmd>ToggleTerm size=80 direction=vertical<Enter>"
+lvim.builtin.which_key.setup.plugins.presets.z = true
 
-vim.keymap.set( 'n', '<leader>tt', ':ToggleTerm size=80 direction=float<Enter>', { silent = true, desc = "[T]oggle[T]erm centered" })
-vim.keymap.set( 'n', '<leader>ts', ':ToggleTerm size=80 direction=vertical<Enter>', { silent = true, desc = "[T]oggleTerm [S]ide" })
-
--- create a new tab
-vim.keymap.set( 'n' , '<leader>..', ':tabnew<Enter>', { silent = true })
--- switch to prev tab
-vim.keymap.set( 'n' , '<leader>.,', ':tabp<Enter>', { silent = true })
--- switch to next tab
-vim.keymap.set( 'n' , '<leader>.p', ':tabn<Enter>', { silent = true })
--- close tab
-vim.keymap.set( 'n' , '<leader>.e', ':tabclose<Enter>', { silent = true })
-
-if vim.g.neovide and vim.fn.has("macos") then
-  -- copy with cmd+C for macos
-  vim.api.nvim_set_keymap('', '<D-c>', '"+y', { noremap = true, silent = true })
-  -- paste with cmd+C for macos, but only in insert mode.
-  vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+if vim.g.neovide then
+  vim.o.guifont = "CaskaydiaCove Nerd Font Mono"
+  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_transparency = 0.9
+  if vim.fn.has("macos") then
+    -- copy with cmd+C for macos
+    vim.api.nvim_set_keymap('', '<D-c>', '"+y', { noremap = true, silent = true })
+    -- paste with cmd+C for macos, but only in insert mode.
+    vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  end
 end
 
